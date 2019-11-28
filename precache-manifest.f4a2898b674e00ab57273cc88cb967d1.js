b8c4bbd0495161974b3668a1c4a268301fa70e3c
Juan Jose Acaiturri-Villa <juan.acaiturri@hotmail.com>
2019-11-28 15:59:01 -0500

    Updates

---
+++ precache-manifest.f4a2898b674e00ab57273cc88cb967d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc038c01a9c55a5909b9e8a1026f535",
+    "url": "/noBarrierChat/index.html"
+  },
+  {
+    "revision": "c4327b34d9933bc61fe4",
+    "url": "/noBarrierChat/static/css/main.a00a0cfc.chunk.css"
+  },
+  {
+    "revision": "2d6407c89d10f0dd3957",
+    "url": "/noBarrierChat/static/js/2.2cc8fe7b.chunk.js"
+  },
+  {
+    "revision": "c4327b34d9933bc61fe4",
+    "url": "/noBarrierChat/static/js/main.f4b7af66.chunk.js"
+  },
+  {
+    "revision": "1c0361805bbf3aab7de8",
+    "url": "/noBarrierChat/static/js/runtime-main.ccc2d64f.js"
+  }
+]);
