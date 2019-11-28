56db8484cf44f845a3e5f8ad6a16ec3f8c56362f
Juan Jose Acaiturri-Villa <juan.acaiturri@hotmail.com>
2019-11-28 16:24:37 -0500

    first commit

--- src/translate.js
+++ src/translate.js
@@ -5,18 +5,14 @@ const translatorApiKey = "trnsl.1.1.20191120T174117Z.30abf07a083257c3.606e1a38fc
 
 function translate(text,sender,reciever,nickname) {
 
-    console.log("starts");
-    console.log(text);
-    console.log(sender);
-    console.log(reciever);
-    console.log(nickname);
-    console.log("ends");
 
     firebase.database().ref(`${reciever}/settings/language`).once("value").then((snapshot)=>{
         
+        const language = snapshot.val();
+        
+        console.log(language,"language here");
         
 
-        const language = snapshot.val();
         axios({
             method:"get",
             url: "https://translate.yandex.net/api/v1.5/tr.json/translate",
